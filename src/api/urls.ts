dc2fe2fd18fd9338496c597df560b6f553307824
Vinisha Yadav <vinishayadav@Mac.lan>
2025-09-23 11:50:53 +0530

    url removed /api

--- src/api/urls.ts
+++ src/api/urls.ts
@@ -4,7 +4,7 @@ const url = {
   deleteUser: "api/user/delete",
   login: "api/auth/login",
   logout: "api/auth/logout",
-  register: "auth/register",
+  register: "/auth/register",
   resetPassword: "api/auth/reset-password",
   changePassword: "api/auth/change-password",
   verifyEmail: "api/auth/verify-email",
