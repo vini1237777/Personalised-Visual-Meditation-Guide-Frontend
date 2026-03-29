8290bcde57a8474cc4aec278d5bcdfcd7146c098
Vinisha Yadav <vinishadv@gmail.com>
2026-03-29 13:05:43 +0530

    feat: add video URLs for background videos hosted on S3

---
+++ src/assets/bg-videos/videoUrls.ts
@@ -0,0 +1,8 @@
+const S3 = "https://soulsync-videos.s3.ap-south-1.amazonaws.com";
+
+export const BG_VIDEOS = {
+  bg1: `${S3}/bg1.mp4`,
+  bg2: `${S3}/bg2.mp4`,
+  bg3: `${S3}/bg3.mp4`,
+  bg4: `${S3}/bg4.mp4`,
+};
